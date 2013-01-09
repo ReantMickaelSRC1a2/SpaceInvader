d1e51245c8f06392d8a691a69338df30d924b6cc
ReantMickaelSRC1a2 <mickael.reant@gmail.com>
2013-01-09 10:00:21 +0100

    Update src/fr/dubois/space/invader/Alien.java

--- src/fr/dubois/space/invader/Alien.java
+++ src/fr/dubois/space/invader/Alien.java
@@ -1,25 +1,22 @@
 package fr.dubois.space.invader;
 
 import android.graphics.Bitmap;
+import android.util.Property;
 import android.view.View;
+
 
 public class Alien extends Sprite {
 
-	public Alien(Bitmap bitmap, float x, float y) {
-		super(bitmap, x, y);
-		// TODO Auto-generated constructor stub
-	}
+public Alien(Bitmap bitmap, float x, float y) {
+super(bitmap, x, y);
+//test
+// TODO Auto-generated constructor stub
+}  
 
-	@Override
-	public void act() {
-		//int droite = spaceInvaderView1.getMaximumSize();
-		// TODO Auto-generated method stub
-		//if (x == (droite-20)){
-		//x=x+5;
-		//}
-	}
+@Override
+public void act() {
+// TODO Auto-generated method stub
+	x=x+2;}
 
-	
 }
-
 
