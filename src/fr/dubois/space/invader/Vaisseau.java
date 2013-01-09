fea8283ea4bfe85004d42baca49d71fe10932a1e
ReantMickaelSRC1a2 <mickael.reant@gmail.com>
2013-01-09 00:59:53 -0800

    Create Vaisseau.java

---
+++ src/fr/dubois/space/invader/Vaisseau.java
@@ -0,0 +1,21 @@
+package fr.dubois.space.invader;
+
+import android.graphics.Bitmap;
+
+public class Vaisseau extends Sprite {
+
+			public Vaisseau(Bitmap bitmap, float x, float y) {
+			super(bitmap, x, y);
+			//test
+			// TODO Auto-generated constructor stub
+			}
+
+			@Override
+			public void act() {
+				// TODO Auto-generated method stub
+				
+			}
+  
+	
+
+}
